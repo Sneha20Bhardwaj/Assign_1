a0d3d867f59d3b8c7d775ba28dca75408c05f8fc
Sneha20Bhardwaj <163544358+Sneha20Bhardwaj@users.noreply.github.com>
2025-06-16 21:27:48 +0530

    First question of Assignment 1

---
+++ Q1.py
@@ -0,0 +1,11 @@
+a=int(input("Enter the first number :"))
+b=int(input("Enter the second number :"))
+print("\n")
+add=a+b
+print("Addition : ",add)
+sub=a-b
+print("Subtraction : ",sub)
+mul=a*b
+print("Multiplication : ",mul)
+div=a/b
+print("Division : ",div)
